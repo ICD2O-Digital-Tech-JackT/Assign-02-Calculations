6c4738914b78ed750c1319ca462233b9b3ca80ae
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-03-27 15:05:04 +0000

    First Edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function DisplayArea(Area){
+  if (document.getElementById("decimals").value){
+    Area=Area.toFixed(document.getElementById("decimals").value)
+  } else{
+    Area=Area.toFixed(5)
+  }
+  document.getElementById("Output").innerHTML="The area of this Cone is "+Area+"cm<sup>2</sup>";
+}
+
+function Calculate(Formula){
+  if (Formula=="Area"){
+    let Height = document.getElementById("height").value
+    let Radius = document.getElementById("radius").value
+    let Area = Math.PI*Math.pow(Radius,2)*(Height/3)
+    DisplayArea(Area)
+  } else{
+    
+  }
+ 
+}
