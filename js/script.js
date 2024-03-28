9c7c3eb54aeccb02218508ed16be12b43eda54a4
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-03-28 15:05:04 +0000

    Made first big edit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
-function DisplayArea(Area){
+function Display(DisplayVar){
   if (document.getElementById("decimals").value){
-    Area=Area.toFixed(document.getElementById("decimals").value)
+      DisplayVar=DisplayVar.toFixed(document.getElementById("decimals").value)
   } else{
-    Area=Area.toFixed(5)
+      DisplayVar=DisplayVar.toFixed(5)
   }
-  document.getElementById("Output").innerHTML="The area of this Cone is "+Area+"cm<sup>2</sup>";
+  document.getElementById("Output").innerHTML="The area of this Cone is "+DisplayVar+"cm<sup>2</sup>";
 }
 
 function Calculate(Formula){
-  if (Formula=="Area"){
-    let Height = document.getElementById("height").value
-    let Radius = document.getElementById("radius").value
-    let Area = Math.PI*Math.pow(Radius,2)*(Height/3)
-    DisplayArea(Area)
+  if (document.getElementById("option-1").checked){
+    Formula="Area"
+  }
+  if (document.getElementById("radius").value &&  document.getElementById("height").value){
+    if (Formula=="Area"){
+      let Height = document.getElementById("height").value
+      let Radius = document.getElementById("radius").value
+      let Area = Math.PI*Math.pow(Radius,2)*(Height/3);
+      Display(Area)
+    } else{
+      let Height = document.getElementById("height").value
+      let Radius = document.getElementById("radius").value
+      let SurfaceArea = (Math.PI*Number(Radius))*(Number(Radius)+Math.sqrt(Math.pow(Radius,2)+Math.pow(Height,2))); //(Math.PI*Radius)*(Radius+Math.sqrt( (Radius*Radius)+(Height*Height) ));
+      Display(SurfaceArea)
+    }
   } else{
-    
+    document.getElementById("Output").innerHTML="Please enter all the values";
   }
  
 }
