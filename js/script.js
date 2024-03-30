b0294e0c05b22a8eb1694640f4fad98df536cf8d
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-03-30 18:32:59 +0000

    dsgyaHGUWHEJRT

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* Used to display the result of the calculations*/
 function Display(DisplayVar){
   if (document.getElementById("decimals").value){
@@ -15,18 +17,17 @@ function Calculate(Formula){
   }
   if (document.getElementById("radius").value &&  document.getElementById("height").value){
     if (Formula=="Area"){
-      let Height = document.getElementById("height").value
-      let Radius = document.getElementById("radius").value
+      let Height = document.getElementById("height").value;
+      let Radius = document.getElementById("radius").value;
       let Area = Math.PI*Math.pow(Radius,2)*(Height/3);
       Display(Area)
     } else{
-      let Height = document.getElementById("height").value
-      let Radius = document.getElementById("radius").value
+      let Height = document.getElementById("height").value;
+      let Radius = document.getElementById("radius").value;
       let SurfaceArea = (Math.PI*Number(Radius))*(Number(Radius)+Math.sqrt(Math.pow(Radius,2)+Math.pow(Height,2))); //(Math.PI*Radius)*(Radius+Math.sqrt( (Radius*Radius)+(Height*Height) ));
       Display(SurfaceArea)
     }
   } else{
     document.getElementById("Output").innerHTML="Please enter all the values";
   }
- 
 }
